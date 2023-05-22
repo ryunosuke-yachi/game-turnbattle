078beaf829fac2e4271e395a47a41087da34ba9d
yuki <yuyak921@gmail.com>
2023-05-22 12:14:52 +0900

    敵のスキル情報を保管するScriptableObjectを作成

---
+++ Assets/Scripts/enemySkillData.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class enemySkillData : ScriptableObject
+{
+    public List<enemySkillInfo> enemySkillInfoList = new List<enemySkillInfo>();
+}
+
+[System.Serializable] 
+public class enemySkillInfo{
+
+
+    public string name;//スキル名
+    public int minMP;//最小MP
+    public int maxMP;//最大MP
+    public string Effect;//スキル効果
+    public int gauge;//ためるゲージの量
+    public int image;//スキルイラスト
+
+}
