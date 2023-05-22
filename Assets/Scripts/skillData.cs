f4df32366d5f177d782f3e866f372ef226b1ccb7
yuki <yuyak921@gmail.com>
2023-05-22 11:22:21 +0900

    プレイヤーのスキル情報を保管するScriptableObjectを作成

---
+++ Assets/Scripts/skillData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class skillData : ScriptableObject
+{
+    public List<skillInfo> skillInfoList = new List<skillInfo>(); 
+}
+
+[System.Serializable]
+public class skillInfo
+{
+
+    public string Name; //スキル名
+    public int MP;//必要MP
+    public string Effect;//スキル効果
+    public int skillGroup;//0 = 攻撃, 1 = 防御, 2 = アイテム
+    public int turnLimit;//ターン制限
+    public bool isSelect;//選択されているか
+    public int image;//スキルイラスト
+
+}
