91d6777df193b43ba84febe4808e7c438b1d090d
602-31\cre <21csc0005ky@edu.sca.ac.jp>
2023-05-27 17:41:26 +0900

    敵のdiceRollスクリプトを作成

--- Assets/Scripts/playerDiceRoll.cs
+++ Assets/Scripts/playerDiceRoll.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class diceRoll : MonoBehaviour
+public class playerDiceRoll : MonoBehaviour
 {
 
     [SerializeField]
