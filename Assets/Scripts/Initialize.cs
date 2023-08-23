2bf4d5ee79c0b8bbbc1164abfbc5e9020f8d4e45
yuki <21csc0005ky@edu.sca.ac.jp>
2023-08-24 06:04:34 +0900

    タイトルシーン起動時にplayerStatusとenemyStatusを初期値に戻す処理を追加

---
+++ Assets/Scripts/Initialize.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Initialize : MonoBehaviour
+{
+    [SerializeField]
+    PlayerStatus playerStatus;
+    [SerializeField]
+    EnemyStatus enemyStatus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerStatus.HP = 450;
+        playerStatus.MP = 0;
+        playerStatus.diceSituation = 0;
+        enemyStatus.HP = 500;
+        enemyStatus.MP = 0;
+        enemyStatus.diceSituation = 0;
+        enemyStatus.gauge = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
