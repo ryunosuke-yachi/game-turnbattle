c5195916ad1c274b594fec977da2fb071b90265d
yuki <21csc0005ky@edu.sca.ac.jp>
2023-08-24 20:03:23 +0900

    TitleSceneからMainGameSceneに遷移する際にステータスを初期化する処理を追加

--- Assets/Scripts/Initialize.cs
+++ Assets/Scripts/Initialize.cs
@@ -12,6 +12,17 @@ public class Initialize : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InitializeStatus()
+    {
         playerStatus.HP = 450;
         playerStatus.MP = 0;
         playerStatus.diceSituation = 0;
@@ -19,11 +30,5 @@ public class Initialize : MonoBehaviour
         enemyStatus.MP = 0;
         enemyStatus.diceSituation = 0;
         enemyStatus.gauge = 0;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
