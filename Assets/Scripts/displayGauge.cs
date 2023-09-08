880fff5128b2ef5f196414476910b717ecd89b22
yuki <21csc0005ky@edu.sca.ac.jp>
2023-09-09 08:45:38 +0900

    敵のスキル情報を表示する処理を追加

--- Assets/Scripts/displayGauge.cs
+++ Assets/Scripts/displayGauge.cs
@@ -10,11 +10,18 @@ public class displayGauge : MonoBehaviour
     [SerializeField] Sprite[] enemySkill;
     [SerializeField] GameObject enemySkillButton;
     [SerializeField] enemySkillData enemyskillData;
+    [SerializeField] GameObject enemySkillInfoUI;
+    [SerializeField] GameObject skillInfoButton;
+    [SerializeField] Text Name;
+    [SerializeField] Text Explanation;
     Image enemySkillButtonImage;
+    Image enemySkillInfoImage;
+
     // Start is called before the first frame update
     void Start()
     {
         enemySkillButtonImage = enemySkillButton.GetComponent<Image>();
+        enemySkillInfoImage = skillInfoButton.GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -63,6 +70,41 @@ public class displayGauge : MonoBehaviour
         {
             enemySkillButton.SetActive(true);
             enemySkillButtonImage.sprite = enemySkill[3];
+        }
+    }
+
+    public void showSkillInfo()
+    {
+        enemySkillInfoUI.SetActive(true);
+        if(enemySkillButtonImage.sprite == enemySkill[0])
+        {
+            enemySkillInfoImage.sprite = enemySkill[0];
+            Name.text = enemyskillData.enemySkillInfoList[0].name;
+            Explanation.text = enemyskillData.enemySkillInfoList[0].Effect;
+        }
+        else if(enemySkillButtonImage.sprite == enemySkill[1])
+        {
+            enemySkillInfoImage.sprite = enemySkill[1];
+            Name.text = enemyskillData.enemySkillInfoList[1].name;
+            Explanation.text = enemyskillData.enemySkillInfoList[1].Effect;
+        }
+        else if (enemySkillButtonImage.sprite == enemySkill[2])
+        {
+            enemySkillInfoImage.sprite = enemySkill[2];
+            Name.text = enemyskillData.enemySkillInfoList[2].name;
+            Explanation.text = enemyskillData.enemySkillInfoList[2].Effect;
+        }
+        else if (enemySkillButtonImage.sprite == enemySkill[3])
+        {
+            enemySkillInfoImage.sprite = enemySkill[3];
+            Name.text = enemyskillData.enemySkillInfoList[3].name;
+            Explanation.text = enemyskillData.enemySkillInfoList[3].Effect;
+        }
+        else if (enemySkillButtonImage.sprite == enemySkill[4])
+        {
+            enemySkillInfoImage.sprite = enemySkill[4];
+            Name.text = enemyskillData.enemySkillInfoList[4].name;
+            Explanation.text = enemyskillData.enemySkillInfoList[4].Effect;
         }
     }
 }
